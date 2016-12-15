a4983595e5cb9c73ec0fd2bc34c0d7a317902d09
Dropee <pedrob1ih@gmail.com>
2016-12-15 11:33:46 +0100

    knowing how to copy files

--- src/mp3metadata/Mp3MetaData.java
+++ src/mp3metadata/Mp3MetaData.java
@@ -2,26 +2,30 @@ package mp3metadata;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.farng.mp3.MP3File;
 import org.farng.mp3.TagException;
 
-
 public class Mp3MetaData {
 
         public static void main(String[] args) {
         try {
-            org.farng.mp3.MP3File mp3= new MP3File(new File("C:\\Users\\pedro\\Desktop\\cancion.mp3"));
+            File song =new File("cancion.mp3");
+            org.farng.mp3.MP3File mp3= new MP3File(song);
             mp3.getID3v2Tag().setSongGenre("xxx");
             mp3.save();
             
-        }
-        catch (IOException ex) {
-            Logger.getLogger(Mp3MetaData.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (TagException ex) {
-            Logger.getLogger(Mp3MetaData.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//            Files.copy(song.toPath(), new File("C:\\Users\\pedro\\Desktop\\copia.mp3").toPath(),StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(song.toPath(), new File("C:\\Users\\pedro\\Desktop\\copia.mp3").toPath());
+            
+        }   catch (IOException ex) {
+                Logger.getLogger(Mp3MetaData.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (TagException ex) {
+                Logger.getLogger(Mp3MetaData.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
     }
     
 }
