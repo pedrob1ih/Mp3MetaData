389a050d76e77999de3c3ec38a6bff432287c12c
pedrob1ih <pedrob1ih@gamail.com>
2016-12-16 14:01:54 +0100

    the scan of files y done but in beta

--- src/mp3metadata/Metadata.java
+++ src/mp3metadata/Metadata.java
@@ -15,6 +15,7 @@ public class Metadata {
     private String musicFolder;
     private ThreadMetadata tM;
     private ArrayList<String> lTypes;
+    private ArrayList<ArrayList> lMp3BT;
     public enum tagType{AlbumTitle, SongComment, AuthorComposer,
         DuplicateFrameId, Identifier, LeadArtist, SongGenre, SongLyric,
         SongTitle, TrackNumberOnAlbum, Size}
@@ -32,13 +33,13 @@ public class Metadata {
         
         for (String lFile : lFiles) {
             if(lFile.endsWith(".mp3") || lFile.endsWith(".flac") ||lFile.endsWith(".aiff")|| lFile.endsWith(".m4a")||lFile.endsWith(".wav")){
-                String pahtSong=musicFolder+"\\"+lFile;
-//                pahtSong=pahtSong.replace("(", "\\(");
-//                pahtSong=pahtSong.replace(")", "\\)");
-                lMP3s.add(new MP3File(new File(pahtSong)));
+                String pahtSong=musicFolder+"/"+lFile;
+                File fileCambiado=eraseParuntesis(pahtSong);
+                //System.out.println("fileCambiado: "+fileCambiado.getAbsolutePath());
+                lMP3s.add(new MP3File(fileCambiado));
             }
             else{
-                scanMp3(musicFolder+"\\"+lFile);
+                scanMp3(musicFolder+"/"+lFile);
             }
         }
     }
@@ -48,14 +49,13 @@ public class Metadata {
         
         for (String lFile : lFiles) {
             if(lFile.endsWith(".mp3") || lFile.endsWith(".flac") ||lFile.endsWith(".aiff")|| lFile.endsWith(".m4a")||lFile.endsWith(".wav")){
-                String pahtSong=musicFolder+"\\"+lFile;
-//                pahtSong=pahtSong.replace("(", "\\(");
-//                pahtSong=pahtSong.replace(")", "\\)");
-                System.out.println("paht "+pahtSong);
-                lMP3s.add(new MP3File(new File(pahtSong)));
+                String pahtSong=musicFolder+"/"+lFile;
+                File fileCambiado=eraseParuntesis(pahtSong);
+                //System.out.println("fileCambiado: "+fileCambiado.getAbsolutePath());
+                lMP3s.add(new MP3File(fileCambiado));
             }
             else{
-                scanMp3(musicFolder+"\\"+lFile);
+                scanMp3(musicFolder+"/"+lFile);
             }
         }
     }
@@ -63,9 +63,15 @@ public class Metadata {
     public void exportBy(tagType by,String paht) throws IOException {
         switch(by){
             case AlbumTitle:
-                scannBy(by);
+                scanTagTypeInFolder(by);
                 for (String lType : lTypes) {
                     getFilesBy(by);
+                    for (ArrayList l : lMp3BT) {
+                        //System.out.println("By : "+by);
+                        for (Object o : l) {
+                            //System.out.println(((MP3File)o).getID3v2Tag().getSongTitle());
+                        }
+                    }
                     //copyFiles(getFilesBy(by));
                 }
                 
@@ -105,7 +111,7 @@ public class Metadata {
         }
     }
     
-    private void scannBy(tagType by){
+    private void scanTagTypeInFolder(tagType by){
         switch(by){
             case AlbumTitle:
                 for (MP3File lMP3 : lMP3s) {
@@ -193,7 +199,7 @@ public class Metadata {
     
     private ArrayList<MP3File> getFilesBy(tagType tT){
         
-        ArrayList<ArrayList> lMp3BT= new ArrayList<>();
+        lMp3BT= new ArrayList<>();
         ArrayList<MP3File> l= new ArrayList<>();
         switch(tT){
                     case AlbumTitle:
@@ -309,21 +315,23 @@ public class Metadata {
         return i;
     }
     
-    public static void showTags(MP3File mp3) {
+    public static String showTags(MP3File mp3) {
         AbstractID3v2 mp3TagsV2=mp3.getID3v2Tag();
-        System.out.println("Album "+mp3TagsV2.getAlbumTitle());
-        System.out.println("Comment "+mp3TagsV2.getSongComment());            
-        System.out.println("getAuthorComposer "+mp3TagsV2.getAuthorComposer());            
-        System.out.println("getDuplicateFrameId "+mp3TagsV2.getDuplicateFrameId());            
-        System.out.println("getIdentifier "+mp3TagsV2.getIdentifier());            
-        System.out.println("getLeadArtist "+mp3TagsV2.getLeadArtist());
-        System.out.println("getSongComment "+mp3TagsV2.getSongComment());
-        System.out.println("getSongGenre "+mp3TagsV2.getSongGenre());
-        System.out.println("getSongLyric "+mp3TagsV2.getSongLyric());
-        System.out.println("getSongTitle "+mp3TagsV2.getSongTitle());
-        System.out.println("getTrackNumberOnAlbum "+mp3TagsV2.getTrackNumberOnAlbum());
-        System.out.println("getYearReleased "+mp3TagsV2.getYearReleased());
-        System.out.println("getSize "+mp3TagsV2.getSize());        
+        String str="";
+        str+="Album "+mp3TagsV2.getAlbumTitle();
+        str+="Comment "+mp3TagsV2.getSongComment();
+        str+="getAuthorComposer "+mp3TagsV2.getAuthorComposer();
+        str+="getDuplicateFrameId "+mp3TagsV2.getDuplicateFrameId();
+        str+="getIdentifier "+mp3TagsV2.getIdentifier(); 
+        str+="getLeadArtist "+mp3TagsV2.getLeadArtist();
+        str+="getSongComment "+mp3TagsV2.getSongComment();
+        str+="getSongGenre "+mp3TagsV2.getSongGenre();
+        str+="getSongLyric "+mp3TagsV2.getSongLyric();
+        str+="getSongTitle "+mp3TagsV2.getSongTitle();
+        str+="getTrackNumberOnAlbum "+mp3TagsV2.getTrackNumberOnAlbum();
+        str+="getYearReleased "+mp3TagsV2.getYearReleased();
+        str+="getSize "+Integer.valueOf(mp3TagsV2.getSize());
+        return str;
     }
     //buscar por metadatos
     public static void searchByTags(String str){
@@ -331,8 +339,36 @@ public class Metadata {
     }
     
     public ArrayList<MP3File> getlMP3s() throws InterruptedException {
-        while(tM.isAlive())
-            Thread.sleep(200);
+        while(tM.isAlive()){
+            Thread.sleep(1000);
+            System.out.println("Cargando discoteca");
+        }
+            
         return lMP3s;
+    }
+    
+    public static File eraseParuntesis(String sFile) throws IOException{
+        File file = new File(sFile);
+        if(sFile.contains("(") || sFile.contains(")") || sFile.contains("[") || sFile.contains("]") || sFile.contains(",")) {
+            sFile=sFile.replace("(", "-");
+            sFile=sFile.replace(")", "-");
+            sFile=sFile.replace("[", "-");
+            sFile=sFile.replace("]", "-");
+            sFile=sFile.replace(",", "");
+            
+            File file2 = new File(sFile);
+
+            if (file2.exists())
+               throw new java.io.IOException("file exists");
+
+            boolean success = file.renameTo(file2);
+
+            if (success) 
+                return file2;
+            
+            return null;
+        }
+        
+        return file;
     }
 }
--- src/mp3metadata/Mp3MetaData.java
+++ src/mp3metadata/Mp3MetaData.java
@@ -1,27 +1,16 @@
 package mp3metadata;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.farng.mp3.MP3File;
-import org.farng.mp3.TagException;
 
 public class Mp3MetaData {
 
         public static void main(String[] args) throws InterruptedException {
-        try {
-            Metadata m = new Metadata("C:\\Users\\pedro\\Desktop\\caperta");
+//        try {
+            Metadata m = new Metadata("/home/pedroaznar/Desktop/dora/musica");
+//            m.exportBy(Metadata.tagType.SongGenre, "");
             for (MP3File mp3 : m.getlMP3s()) {
-                System.out.println(mp3.getID3v2Tag().getSongGenre());
+                System.out.println(mp3.getMp3file().toPath());
             }
-            
-            
-
-
-//            Metadata m= new Metadata("D:\\musica\\penCoche\\nuevas");
-//            for (MP3File mp3 : m.getlMP3s()) {
-//                System.out.println(mp3.getMp3file().toPath());
-//            }
 
 //            File song =new File("cancion.mp3");
 //            org.farng.mp3.MP3File mp3= new MP3File(song);
@@ -35,10 +24,10 @@ public class Mp3MetaData {
 //            Files.copy(song.toPath(), new File("C:\\Users\\pedro\\Desktop\\copia.mp3").toPath(),StandardCopyOption.REPLACE_EXISTING);
 //            Files.copy(song.toPath(), new File("C:\\Users\\pedro\\Desktop\\copia.mp3").toPath());
             
-        } catch (InterruptedException ex) {
-                Logger.getLogger(Mp3MetaData.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        
-    }
+//        catch (IOException ex) {
+//                Logger.getLogger(Mp3MetaData.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+            System.out.println("main finalizado");
+        }
     
 }
